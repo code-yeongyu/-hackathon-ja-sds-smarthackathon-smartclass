70a1aadb88d2db03d4c008e0581d6352069a4dde
YeonGyu <code.yeon.gyu@gmail.com>
2019-08-07 02:54:23 +0900

    Fix typo

--- backend/class_status/models.py
+++ backend/class_status/models.py
@@ -6,5 +6,5 @@ from imagekit.models import ImageSpecField
 class Status(models.Model):
     arduino_id = models.IntegerField(null=False, default=-1)
     image = models.ImageField(upload_to='./detect/%Y%m%d/', )
-    gas_quality = models.Floatfield(null=False)
+    gas_quality = models.FloatField(null=False)
     air_quality = models.FloatField(null=False)
