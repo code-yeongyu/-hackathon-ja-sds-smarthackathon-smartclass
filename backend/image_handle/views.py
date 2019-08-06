a67156aa0a51b22d57a0d197ee25de8bdc7cf2c9
YeonGyu <code.yeon.gyu@gmail.com>
2019-08-06 23:25:57 +0900

    Add swagger

--- backend/image_handle/views.py
+++ backend/image_handle/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from image_handle.serializers import ImageSerializer
 from django.shortcuts import get_object_or_404
 from image_handle.models import Image
-#import image_handle.yolo as yolo
+import image_handle.yolo as yolo
 
 
 @api_view(['POST'])
@@ -17,6 +17,5 @@ def create_image(request):
         serializer.save()
         path = "./" + get_object_or_404(Image,
                                         pk=serializer.data['id']).image.url
-        return Response(status=HTTP_200_OK)
-        #return Response(yolo.AnyBodyHere(path), status=status.HTTP_201_CREATED)
+        return Response(yolo.AnyBodyHere(path), status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
