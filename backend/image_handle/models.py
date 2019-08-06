ac1ac6151d5597bcf7d58b4fa163b4e6382dd54e
YeonGyu <code.yeon.gyu@gmail.com>
2019-08-06 21:38:42 +0900

    Add checking human feature

--- backend/image_handle/models.py
+++ backend/image_handle/models.py
@@ -4,5 +4,5 @@ from imagekit.models import ImageSpecField
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='static/uploaded/images/%Y/%m/%d/', )
+    image = models.ImageField(upload_to='./detect/%Y%m%d/', )
     arduino_id = models.IntegerField(null=False, default=-1)
--- backend/image_handle/views.py
+++ backend/image_handle/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from image_handle.serializers import ImageSerializer
+from django.shortcuts import get_object_or_404
+from image_handle.models import Image
+import image_handle.yolo as yolo
 
 
 @api_view(['POST'])
@@ -12,6 +15,7 @@ def create_image(request):
     serializer = ImageSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        # image handling logic
-        return Response(status=status.HTTP_201_CREATED)
+        path = "./" + get_object_or_404(
+            Image, pk=serializer.data['id']).image.url, 'rb'
+        return Response(yolo.AnyBodyHere(path), status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
