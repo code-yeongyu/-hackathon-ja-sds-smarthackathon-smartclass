1eb6eee25570b0e051bcf3c03077662ab1727966
YeonGyu <code.yeon.gyu@gmail.com>
2019-08-06 22:38:07 +0900

    Fix bug
    Fix wrong syntax

--- backend/image_handle/views.py
+++ backend/image_handle/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from image_handle.serializers import ImageSerializer
 from django.shortcuts import get_object_or_404
 from image_handle.models import Image
-import image_handle.yolo as yolo
+#import image_handle.yolo as yolo
 
 
 @api_view(['POST'])
@@ -15,7 +15,8 @@ def create_image(request):
     serializer = ImageSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        path = "./" + get_object_or_404(
-            Image, pk=serializer.data['id']).image.url, 'rb'
-        return Response(yolo.AnyBodyHere(path), status=status.HTTP_201_CREATED)
+        path = "./" + get_object_or_404(Image,
+                                        pk=serializer.data['id']).image.url
+        return Response(status=HTTP_200_OK)
+        #return Response(yolo.AnyBodyHere(path), status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
