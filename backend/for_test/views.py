ff2758aa3f618340d85d193eaf3467ccd39d0eea
YeonGyu <code.yeon.gyu@gmail.com>
2019-08-06 19:26:20 +0900

    Remove miscellaneous files

--- backend/for_test/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-@api_view(['GET'])
-def test(request):
-    return Response({'ok': request.GET['id']}, status=status.HTTP_200_OK)
