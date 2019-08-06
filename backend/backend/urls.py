c8deed466e104543811d5f8217471e5e944ea415
YeonGyu <code.yeon.gyu@gmail.com>
2019-08-06 23:34:42 +0900

    Fix a bug
    permission is now imported

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
+from rest_framework import permissions
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 
